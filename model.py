2aba751d7a3a05face01b1a85a2f2113ffb7a2a8
Akshay L Chandra <14245491+acl21@users.noreply.github.com>
2022-01-18 17:33:43 +0100

    Updated model.py approrpiately
    The `batch_first=True` is the killer. Once that is clear, every change I made here should be quite clear to you. If not, you could either reply there or reply here. I will answer. :)

--- model.py
+++ model.py
@@ -32,7 +32,8 @@ class DecoderRNN(nn.Module):
 
         # the LSTM takes embedded word vectors (of a specified size) as inputs 
         # and outputs hidden states of size hidden_size
-        self.lstm = nn.LSTM(embed_size, hidden_size)
+        # Reviewer Note: I have explained this in the comment but this will help - https://discuss.pytorch.org/t/could-someone-explain-batch-first-true-in-lstm/15402
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers=num_layers, batch_first=True)
 
         # the linear layer that maps the hidden state output dimension 
         # to the number of captions we want as output, vocab_size
@@ -57,22 +58,28 @@ class DecoderRNN(nn.Module):
         print(features.size())
         print(captions.size())
         #features_long=features.long()
-        features = features.view(-1, 1, self.embed_size)
+        # Reviewer Note: There is an elegant way to do what you need with features
+        features = features.unsqueeze(1)
         #embeds = self.word_embeddings(captions)
         embeds = self.word_embeddings(captions[:,:-1])
         print(embeds.shape)
-        stacks=torch.cat([features, embeds], dim=1)
+        # Reviewer Note: () are healthier than []. Always ;)
+        stacks=torch.cat((features, embeds), dim=1)
         print(stacks.shape)        
         # get the output and hidden state by passing the lstm over our word embeddings
         # the lstm takes in our embeddings and hiddent state
-        lstm_out, self.hidden = self.lstm(
-            stacks.view(stacks.shape[0],stacks.shape[1], -1), self.hidden)
+        # Reviewer Note: You have already unsqueezed your features, set batch_first=True so the concatenation is perfect.
+        # No need to make hidden a class object. It is unused. However, in sample method you will do it iterantively so there you pass
+        # the hidden state to the lstm. Here we are doing it all in one go. I hope that is clear. :) 
+        lstm_out, lstm_hidden = self.lstm(stacks)
         
         # get the scores for the most likely tag for a word
-        caption_outputs = self.hidden2caption(lstm_out.view(len(stacks), -1))
-        caption_scores = F.log_softmax(caption_outputs, dim=1)
+        caption_outputs = self.hidden2caption(lstm_out)
+        # Reviewer Note: You DO NOT have to do this since your loss function handles this. If you read the 
+        # PyTorch documentation you will notice this - "The input is expected to contain raw, unnormalized scores for each class."
+        #caption_scores = F.log_softmax(caption_outputs, dim=1)
         
-        return caption_scores
+        return caption_output
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
